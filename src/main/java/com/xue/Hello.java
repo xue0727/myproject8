1c4baac86b2d1a56333f3ea8071bf026341fe138
薛勻婷 <cary910727@gmail.com>
2022-06-10 11:53:14 +0800

    Hello world with class Person

---
+++ src/main/java/com/xue/Hello.java
@@ -0,0 +1,9 @@
+package com.xue;
+
+public class Hello {
+    public static void main(String[] args) {
+        //System.out.println("Hello");
+        Person p = new Person();
+        p.greating();
+    }
+}
---
+++ src/main/java/com/xue/Person.java
@@ -0,0 +1,7 @@
+package com.xue;
+
+public class Person {
+    public void greating(){
+        System.out.println("Good day!");
+    }
+}
