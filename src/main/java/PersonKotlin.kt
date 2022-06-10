e67885eba54f0ff4e408fdcd1f2f83341b76810e
薛勻婷 <cary910727@gmail.com>
2022-06-10 12:57:43 +0800

    Hello Kotlin

---
+++ src/main/java/PersonKotlin.kt
@@ -0,0 +1,15 @@
+package com.xue
+
+//分號可打可不打
+class PersonKotlin {
+    fun greeting(){
+        println("Hello Kotlin!")
+    }
+}
+
+//1st level 第一層
+fun main() {
+    val p = PersonKotlin()
+    p.greeting()
+}
+//fun main(args: Array<String>) {}
